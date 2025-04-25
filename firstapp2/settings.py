73a4c918f8643e4d1091ffbe8c2e2f6673ed4f8a
S Chandra Sekhar <kingchandus143@gmail.com>
2025-04-26 02:09:50 +0530

    Update settings.py

--- firstapp2/settings.py
+++ firstapp2/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import pymysql
+import os
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-%q8t1jbvhgd-9-ue33#$puz81u5qdio@ja3i4jj_!1ua2o!pdn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['djangotesting-zg9u.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -39,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'firstdemoapp2',
     'corsheaders',
+    'firstdemoapp2',
 ]
 
 MIDDLEWARE = [
@@ -52,8 +53,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
+
 ROOT_URLCONF = 'firstapp2.urls'
 
 TEMPLATES = [
@@ -84,24 +87,10 @@ DATABASES = {
         'NAME': 'loginsignup',
         'USER': 'kingchadnu',
         'PASSWORD': 'ubvrCG3uuJOVJP5X4XKnQ1beEQWzn5OH',
-        'HOST': 'dpg-d00tlrjuibrs73eqo6pg-a.oregon-postgres.render.com',
+        'HOST': 'dpg-d00tlrjuibrs73eqo6pg-a.oregon-postgres.render.com',  # external URL
         'PORT': '5432',
     }
 }
-
-'''DATABASES2 = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'flutterapp',
-        'USER': 'root',
-        'PASSWORD': 'King_chandu_s143',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}'''
-
-
-
 
 
 # Password validation
@@ -139,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
